7b8b7f402ea8d94fce7209e87b2e41f966dd58cc
ffernandorl <ffernandorl@gmail.com>
2016-08-09 12:55:45 -0300

    Validação do JSON Empregado

--- empregado.class.php
+++ empregado.class.php
@@ -1,19 +1,27 @@
 <?php
 class Empregado{
 	//Validação do  JSON
-	public function ValidaJson($insertEmpregado, $database){
-		method_exists($insertEmpregado, "RegistroEmpregado");
-		$v_err["numLivro"] = $insertLivro["numLivro"] ? null : "err numLivro";
-		
-
+	public function ValidaJson($insertEmpregado){
+		$v_err["RegistroEmpregado"] = method_exists($insertEmpregado, "RegistroEmpregado")) ? null : "err RegistroEmpregado";
+		$v_err["CaracFisicas"] = method_exists($insertEmpregado, "CaracFisicas")) ? null : "err CaracFisicas";
+		$v_err["Contrato"] = method_exists($insertEmpregado, "Contrato")) ? null : "err Contrato";
+		$v_err["SituacaoFGTS"] = method_exists($insertEmpregado, "SituacaoFGTS")) ? null : "err SituacaoFGTS";
+		$v_err["Estrangeiros"] = method_exists($insertEmpregado, "Estrangeiros")) ? null : "err Estrangeiros";
+		$v_err["PISBeneficiarios"] = method_exists($insertEmpregado, "PISBeneficiarios")) ? null : "err PISBeneficiarios";
+		$v_err["Beneficiarios"] = method_exists($insertEmpregado, "Beneficiarios")) ? null : "err Beneficiarios";
+		$v_err["Salario"] = method_exists($insertEmpregado, "Salario")) ? null : "err Salario";
+		$v_err["Cargo"] = method_exists($insertEmpregado, "Cargo")) ? null : "err Cargo";
+		$v_err["ContribSindical"] = method_exists($insertEmpregado, "ContribSindical")) ? null : "err ContribSindical";
+		$v_err["ADP"] = method_exists($insertEmpregado, "ADP")) ? null : "err ADP";
+		$v_err["Ferias"] = method_exists($insertEmpregado, "Ferias")) ? null : "err Ferias";
 		return $v_err;
 	}
 	//Função para inserir todas as 12 tabelas do empregado
 	public function InserirEmpregado($jsonEmpregado, $database){
 		$insertEmpregado = json_decode($jsonEmpregado); //Decodificando o JSON
-		if (json_last_error() != 0) return json_last_error(); //testa se houve erro no parsing
+		if (json_last_error() != 0) return json_last_error(); //testa se houve erro no parsingContrato
 		//Validando o JSON
-		$v_err = $this->ValidaJson($insertEmpregado, $database);
+		$v_err = $this->ValidaJson($insertEmpregado);
 		foreach ($v_err as $k => $v)
 			if($v) return $v_err;
 		//Inserindo no BD
@@ -30,8 +38,7 @@ class Empregado{
 			$database->insert("Cargo", (array) $insertEmpregado->Cargo);
 			$database->insert("ContribSindical", (array) $insertEmpregado->ContribSindical);
 			$database->insert("ADP", (array) $insertEmpregado->ADP);
-			$database->insert("Ferias", (array) $insertEmpregado->Ferias);
-		
+			$database->insert("Ferias", (array) $insertEmpregado->Ferias);	
 		//Avaliação de possivel erro e retorno da função
 		$e = $database->error();
 		if($e[1] == null){
