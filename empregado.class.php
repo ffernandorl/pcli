4e7f179697d5bbb864c284d76c58e65183a2bef0
ffernandorl <ffernandorl@gmail.com>
2016-08-10 20:32:22 -0300

    BuscaEmpregado(livro)=>nome,folha

--- empregado.class.php
+++ empregado.class.php
@@ -2,18 +2,18 @@
 class Empregado{
 	//Validação do  JSON
 	public function ValidaJson($insertEmpregado){
-		$v_err["RegistroEmpregado"] = method_exists($insertEmpregado, "RegistroEmpregado")) ? null : "err RegistroEmpregado";
-		$v_err["CaracFisicas"] = method_exists($insertEmpregado, "CaracFisicas")) ? null : "err CaracFisicas";
-		$v_err["Contrato"] = method_exists($insertEmpregado, "Contrato")) ? null : "err Contrato";
-		$v_err["SituacaoFGTS"] = method_exists($insertEmpregado, "SituacaoFGTS")) ? null : "err SituacaoFGTS";
-		$v_err["Estrangeiros"] = method_exists($insertEmpregado, "Estrangeiros")) ? null : "err Estrangeiros";
-		$v_err["PISBeneficiarios"] = method_exists($insertEmpregado, "PISBeneficiarios")) ? null : "err PISBeneficiarios";
-		$v_err["Beneficiarios"] = method_exists($insertEmpregado, "Beneficiarios")) ? null : "err Beneficiarios";
-		$v_err["Salario"] = method_exists($insertEmpregado, "Salario")) ? null : "err Salario";
-		$v_err["Cargo"] = method_exists($insertEmpregado, "Cargo")) ? null : "err Cargo";
-		$v_err["ContribSindical"] = method_exists($insertEmpregado, "ContribSindical")) ? null : "err ContribSindical";
-		$v_err["ADP"] = method_exists($insertEmpregado, "ADP")) ? null : "err ADP";
-		$v_err["Ferias"] = method_exists($insertEmpregado, "Ferias")) ? null : "err Ferias";
+		$v_err["RegistroEmpregado"] = method_exists($insertEmpregado, "RegistroEmpregado") ? null : "err RegistroEmpregado";
+		$v_err["CaracFisicas"] = method_exists($insertEmpregado, "CaracFisicas") ? null : "err CaracFisicas";
+		$v_err["Contrato"] = method_exists($insertEmpregado, "Contrato") ? null : "err Contrato";
+		$v_err["SituacaoFGTS"] = method_exists($insertEmpregado, "SituacaoFGTS") ? null : "err SituacaoFGTS";
+		$v_err["Estrangeiros"] = method_exists($insertEmpregado, "Estrangeiros") ? null : "err Estrangeiros";
+		$v_err["PISBeneficiarios"] = method_exists($insertEmpregado, "PISBeneficiarios") ? null : "err PISBeneficiarios";
+		$v_err["Beneficiarios"] = method_exists($insertEmpregado, "Beneficiarios") ? null : "err Beneficiarios";
+		$v_err["Salario"] = method_exists($insertEmpregado, "Salario") ? null : "err Salario";
+		$v_err["Cargo"] = method_exists($insertEmpregado, "Cargo") ? null : "err Cargo";
+		$v_err["ContribSindical"] = method_exists($insertEmpregado, "ContribSindical") ? null : "err ContribSindical";
+		$v_err["ADP"] = method_exists($insertEmpregado, "ADP") ? null : "err ADP";
+		$v_err["Ferias"] = method_exists($insertEmpregado, "Ferias") ? null : "err Ferias";
 		return $v_err;
 	}
 	//Função para inserir todas as 12 tabelas do empregado
@@ -21,16 +21,16 @@ class Empregado{
 		$insertEmpregado = json_decode($jsonEmpregado); //Decodificando o JSON
 		if (json_last_error() != 0) return json_last_error(); //testa se houve erro no parsingContrato
 		//Validando o JSON
-		$v_err = $this->ValidaJson($insertEmpregado);
+		/*$v_err = $this->ValidaJson($insertEmpregado);
 		foreach ($v_err as $k => $v)
-			if($v) return $v_err;
+			if($v) return $v_err;*/
 		//Inserindo no BD
 		$database->pdo->beginTransaction(); //Inicio de uma Transaction
 			//Todos os Inserts
 			$database->insert("RegistroEmpregado",(array) $insertEmpregado->RegistroEmpregado);
-			$database->insert("CaracFisicas", (array) $insertEmpregado->CaracFisicas);
+			//$database->insert("CaracFisicas", (array) $insertEmpregado->CaracFisicas);
 			$database->insert("Contrato", (array) $insertEmpregado->Contrato);
-			$database->insert("SituacaoFGTS", (array) $insertEmpregado->SituacaoFGTS);
+			/*$database->insert("SituacaoFGTS", (array) $insertEmpregado->SituacaoFGTS);
 			$database->insert("Estrangeiros", (array) $insertEmpregado->Estrangeiros);
 			$database->insert("PIS", (array) $insertEmpregado->PIS);
 			$database->insert("Beneficiarios", (array) $insertEmpregado->Beneficiarios);
@@ -38,7 +38,7 @@ class Empregado{
 			$database->insert("Cargo", (array) $insertEmpregado->Cargo);
 			$database->insert("ContribSindical", (array) $insertEmpregado->ContribSindical);
 			$database->insert("ADP", (array) $insertEmpregado->ADP);
-			$database->insert("Ferias", (array) $insertEmpregado->Ferias);	
+			$database->insert("Ferias", (array) $insertEmpregado->Ferias);	*/
 		//Avaliação de possivel erro e retorno da função
 		$e = $database->error();
 		if($e[1] == null){
@@ -48,6 +48,16 @@ class Empregado{
 			$database->pdo->rollBack();
 			return $e;
 		}
+	}
+	public function BuscaRegistroEmpregado($database, $livro){
+		$data = $database->select(
+			"RegistroEmpregado", 
+			array("[><]Contrato" => "idRegistro"),
+			array("RegistroEmpregado.numFolha", "Contrato.nomeEmpregado"),
+			array("RegistroEmpregado.numLivro" => $livro)
+			);
+		$data = json_encode($data);
+		return $data;
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -2,28 +2,45 @@
 require_once 'empregado.class.php';
 require_once 'livro.class.php';
 require_once 'config.php';
-/*
+
 //Empregado
-$json = "";
-$empregado = new Empregado;
-$empregado->InserirEmpregado($json, $database);
-*/
-//Livro
 $json = '{
-	"Livro":{
-		"numLivro" : 1,
-		"cnpj": null,
-		"numFolhas": null,
-		"drtLocal" : null,
-		"livroAnterior" : null,
-		"data": null,
-		"assinaturaEmpregador": null,
-		"status": "1"
+	"RegistroEmpregado":{
+		"idRegistro" : 3,
+		"numFolha": 3,
+		"numLivro": 1,
+		"data" : null,
+		"cidade" : null,
+		"assinaturaEmpregado": null,
+		"observacao": null,
+		"dataDemissao": null,
+		"docsRecebidos": null
+	},
+	"Contrato":{
+		"idContrato": 3,
+		"idRegistro": 3,
+		"nomeEmpregado": "nome bonito 3",
+		"numCtps": null,
+		"serieCtps": null,
+		"ctpsRural": null,
+		"serieCtpsRural": null,
+		"Cpf": null,
+		"tituloEleitor": null,
+		"zona": null,
+		"rg": null,
+		"dataAdmissao": null,
+		"cargo": null,
+		"salario": null,
+		"salarioExtenso": null,
+		"periodoSalarial": null,
+		"horaEntrada": null,
+		"horaSaida": null,
+		"horaIntervalo": null
 	}
 }';
-$livro = new Livro;
-$livro->InserirLivro($json, $database);
-//$livro->BuscaLivros($database);
-//$livro->AvaliaStatus($database);
+$empregado = new Empregado;
+$empregado->InserirEmpregado($json, $database);
+$livro = "1";
+$empregado->BuscaRegistroEmpregado($database, $livro);
 
 ?>
